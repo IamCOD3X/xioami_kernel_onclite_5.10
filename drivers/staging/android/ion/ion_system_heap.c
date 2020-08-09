b70c057c303cdf0dcd520face0f6b2e5c83cf296
Dhina17 <dhinalogu@gmail.com>
2020-08-09 23:29:17 +0530

    drivers: staging: android: Import Xiaomi changes
    Change-Id: Iabf3095d20d588b889f26e588a4e31be4faf8665

--- drivers/staging/android/ion/ion_system_heap.c
+++ drivers/staging/android/ion/ion_system_heap.c
@@ -22,6 +22,12 @@ static gfp_t high_order_gfp_flags = (GFP_HIGHUSER | __GFP_ZERO | __GFP_NOWARN |
 				     __GFP_NORETRY) & ~__GFP_RECLAIM;
 static gfp_t low_order_gfp_flags  = GFP_HIGHUSER | __GFP_ZERO;
 static const unsigned int orders[] = {8, 4, 0};
+#else
+static const unsigned int orders[] = {4, 0};
+#endif
+#else
+static const unsigned int orders[] = {0};
+#endif
 
 static int order_to_index(unsigned int order)
 {
